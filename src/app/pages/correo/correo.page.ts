7f19563c62ddf281758e8b6ebf62bab9d5eb6118
Roberto Vergara <robertovergara157@gmail.com>
2023-09-26 20:39:13 -0300

    toastController agregado

---
+++ src/app/pages/correo/correo.page.ts
@@ -0,0 +1,56 @@
+
+import { Router, NavigationExtras } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-correo',
+  templateUrl: './correo.page.html',
+  styleUrls: ['./correo.page.scss'],
+})
+export class CorreoPage implements OnInit {
+
+  public correo: string = '';
+
+  constructor(private router: Router,private toastController: ToastController) { }
+
+  ngOnInit() {
+  }
+
+  public async  ingresarPaginaValidarRespuestaSecreta(): Promise<void> {
+    
+    const usuario = new Usuario("", "","","","");
+    const usuarioEncontrado = usuario.buscarUsuarioPorCorreo(this.correo);
+    if (!usuarioEncontrado) {
+      // this.toastController.create({
+      //   message:'EL CORREO NO EXISTE DENTRO DE LAS CUENTAS VALIDAS DEL SISTEMA',
+      //   position:"middle",
+      //   duration:2000
+      // }).then(e=>e.present());
+
+      // toastController.create devuelve un objeto y uno de sus metodos es present el cual lo muestra en la pantalla
+      // lo podemos usar con promesas ya que el then atrapa el valor del return lo pone en una nueva variable y se puede
+      // usar su metodo, con el async y await esperamos el resultado lo asignamos a una variable y esa variable contiene los
+      // metodos y ahi usamos el present()
+
+      const asa= await this.toastController.create({
+        message:'EL CORREO NO EXISTE DENTRO DE LAS CUENTAS VALIDAS DEL SISTEMA',
+        position:"middle",
+        duration:2000
+      });
+      asa.present();
+
+      // 2 formas de promesas diferentes
+    }
+    else {
+      const navigationExtras: NavigationExtras = {
+        state: {
+          usuario: usuarioEncontrado
+        }
+      };
+      this.router.navigate(['/pregunta'], navigationExtras);
+    }
+  }
+
+}
